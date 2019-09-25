30645e96761ba47ec508e2ecd8d12191207f434c
luckyxkw <cocolonel.x@gmail.com>
2019-09-25 10:31:20 +0800

    update email template

--- mailsender/mailsender.go
+++ mailsender/mailsender.go
@@ -6,13 +6,15 @@ import (
     "net/smtp"
     "strings"
 
-    m "github.com/AishwaryaRK/tweats/datamodel"
+    model "github.com/AishwaryaRK/tweats/datamodel"
+    matcher "github.com/AishwaryaRK/tweats/matcher"
 )
 
 const senderMail = "*******@gmail.com"
 const senderCredential = "******"
 const smtpHost = "smtp.gmail.com"
 const smtpURI = "smtp.gmail.com:587"
+const twitterMailSuffix = "@twitter.com"
 
 const mailMsgFormat = `From: %s
 To: %s
@@ -20,23 +22,34 @@ Subject: %s
 
 %s`
 const mailSubject = "[Tweats] Lunch Buddy"
-const mailBodyFormat = `Hi Tweeps,
+const mailBodyFormat = `Hello %s,
 
-#Some greetings here#
+Thank you for signing up to SG TwEATS. We found your lunch buddy! Before you start: Please make sure you’ve read and understood the guidelines and best practices at go/SgTwEATS.
 
-regards,
-Tweats Team`
+Your common interests are: %s.
+
+Hope you have a fun & enjoyable time together! Have questions? Need advice? Contact the admin via jiliannew@twitter.com.
+
+Regards,
+TwEATS team
+`
+
+// Send sends emails for one Match
+func Send(match matcher.Match) {
+    send(match.MatchedTweeps, match.MatchedInterest)
+}
 
 // Send sends emails to tweeps
-func Send(tweeps []m.Tweep) {
+func send(tweeps []model.Tweep, interest string) {
     receiverArr := make([]string, len(tweeps))
+    receiverNames := ""
     for index, tweep := range tweeps {
-        receiverArr[index] = tweep.LDAP
+        receiverArr[index] = tweep.LDAP + twitterMailSuffix
     }
-
     receiverStr := strings.Join(receiverArr, ",")
 
-    msg := fmt.Sprintf(mailMsgFormat, senderMail, receiverStr, mailSubject, "TODO: replace body")
+    mailBody := fmt.Sprintf(mailBodyFormat, receiverNames, interest)
+    msg := fmt.Sprintf(mailMsgFormat, senderMail, receiverStr, mailSubject, mailBody)
     auth := smtp.PlainAuth("", senderMail, senderCredential, smtpHost)
     err := smtp.SendMail(smtpURI, auth, senderMail, receiverArr, []byte(msg))
 
